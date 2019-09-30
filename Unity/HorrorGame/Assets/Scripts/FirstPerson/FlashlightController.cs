7b3960457b8573a8903f70f73fb560e1aa5e95e7
Jeff Hardy <jeffthardy@gmail.com>
2019-09-30 14:09:17 -0600

    adding toggle on flashlight with audio

---
+++ Unity/HorrorGame/Assets/Scripts/FirstPerson/FlashlightController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopZombies
+{
+    public class FlashlightController : MonoBehaviour
+    {
+        public float maxIntensity = 3;
+        public bool isOn = true;
+        public float toggleTime = 0.2f;
+        public AudioClip toggleSound;
+
+        private AudioSource audioSource;
+
+        private Light flashlight;
+        private float availableTime = 0;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            flashlight = GetComponent<Light>();
+            audioSource = GetComponent<AudioSource>();
+
+            if (isOn)
+                flashlight.intensity = maxIntensity;
+            else
+                flashlight.intensity = 0;
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void toggleFlashlight()
+        {
+            if (Time.time > availableTime)
+            {
+                availableTime = Time.time + toggleTime;
+                audioSource.PlayOneShot(toggleSound);
+
+                if (isOn)
+                {
+                    flashlight.intensity = 0;
+                    isOn = false;
+                }
+                else
+                {
+                    flashlight.intensity = maxIntensity;
+                    isOn = true;
+                }
+            }
+
+        }
+    }
+}
