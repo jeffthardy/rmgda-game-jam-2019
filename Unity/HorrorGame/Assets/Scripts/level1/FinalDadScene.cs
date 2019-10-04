9b8eeef8c1dded0943571503d9c17db0969418d4
Jeff Hardy <jeffthardy@gmail.com>
2019-10-03 19:27:34 -0600

    jeff scene has full game progression, missing enemies and tv

---
+++ Unity/HorrorGame/Assets/Scripts/level1/FinalDadScene.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopZombies
+{
+    public class FinalDadScene : MonoBehaviour
+    {
+        public GameObject exitDoor1;
+        public GameObject exitDoor2;
+        public AudioClip dadGoodbyeClip;
+        public float postAudioDoorDelay = 1.0f;
+
+        private bool goodbyeHasHappened = false;
+
+
+        private AudioSource dadGoodbyeAudioSource;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            dadGoodbyeAudioSource = GetComponent<AudioSource>();
+
+            exitDoor1.GetComponent<DoorController>().DisableDoor();
+            exitDoor2.GetComponent<DoorController>().DisableDoor();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.GetComponent<FPSController>() != null)
+            {
+                if (!goodbyeHasHappened)
+                {
+                    //Debug.Log("Final Goodbyes");
+                    StartCoroutine(PlayFinalDadWords());
+                }
+            }
+        }
+
+        IEnumerator PlayFinalDadWords()
+        {
+            goodbyeHasHappened = true;
+            dadGoodbyeAudioSource.PlayOneShot(dadGoodbyeClip);
+            enableExit();
+            yield return new WaitForSeconds(dadGoodbyeClip.length + postAudioDoorDelay);
+
+
+        }
+
+        void enableExit()
+        {
+            exitDoor1.GetComponent<DoorController>().EnableDoor();
+            exitDoor2.GetComponent<DoorController>().EnableDoor();
+            exitDoor1.GetComponent<DoorController>().Use();
+            exitDoor2.GetComponent<DoorController>().Use();
+        }
+    }
+}
