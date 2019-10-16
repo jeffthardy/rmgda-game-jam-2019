cb3c781e71c53d96b897b9eebbe7b22e1f4db9d4
Tom <tomblind@users.noreply.github.com>
2019-10-16 14:15:45 -0600

    fixing code that was stomped in merge

--- Unity/HorrorGame/Assets/Scripts/NightmareController.cs
+++ Unity/HorrorGame/Assets/Scripts/NightmareController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 
 namespace TopZombies
@@ -20,6 +21,7 @@ namespace TopZombies
         public bool nightmareMode = false;
 
         private Color initialAmbientLight;
+        public UnityEvent[] switchAction;
 
 
 
@@ -62,6 +64,12 @@ namespace TopZombies
             StartCoroutine(ToggleLightsTo(false, flickerTime, nightmareAudio));
             //SetGlobalLightActive(false);
             setGlobalWorldTextureActive(false);
+
+            for(int i=0;i< switchAction.Length;i++)
+                if (switchAction.Length > i && switchAction[i] != null)
+                {
+                    switchAction[i].Invoke();
+                }
         }
 
             public void SwitchToDream()
@@ -75,6 +83,12 @@ namespace TopZombies
             StartCoroutine(ToggleLightsTo(true, 0.5f, null));
             //SetGlobalLightActive(true);
             setGlobalWorldTextureActive(true);
+
+            for (int i = 0; i < switchAction.Length; i++)
+                if (switchAction.Length > i && switchAction[i] != null)
+                {
+                    switchAction[i].Invoke();
+                }
         }
 
 
