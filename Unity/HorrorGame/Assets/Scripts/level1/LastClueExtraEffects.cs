b49f5f86394209f71dbb4c392ce2b164a4bdf5e3
Jeff Hardy <jeffthardy@gmail.com>
2019-10-16 09:50:49 -0600

    last bit of story direction from dining to chase

---
+++ Unity/HorrorGame/Assets/Scripts/level1/LastClueExtraEffects.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopZombies
+{
+
+    public class LastClueExtraEffects : MonoBehaviour
+    {
+        public DoorController kitchenDoor;
+        public DoorController livingRoomDoor;
+
+        private Light flashingLight;
+        private AudioSource scareAudioSource;
+        private PlaySeriesOfAudioClips playSeriesOfAudioClips;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            scareAudioSource = GetComponent<AudioSource>();
+            flashingLight = GetComponentInChildren<Light>();
+            playSeriesOfAudioClips = GetComponent<PlaySeriesOfAudioClips>();
+            flashingLight.enabled = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void TriggerEndSequence()
+        {
+            if (kitchenDoor.GetComponent<DoorController>().isOpen)
+                kitchenDoor.GetComponent<DoorController>().Use();
+            kitchenDoor.GetComponent<DoorController>().DisableDoor();
+
+            if (livingRoomDoor.GetComponent<DoorController>().isOpen)
+                livingRoomDoor.GetComponent<DoorController>().Use();
+            livingRoomDoor.GetComponent<DoorController>().DisableDoor();
+
+            StartCoroutine(FlashingLightsAndDoor());
+        }
+
+        IEnumerator FlashingLightsAndDoor()
+        {
+            playSeriesOfAudioClips.PlaySeries();
+            StartCoroutine(FlashLights());
+            yield return new WaitForSeconds(playSeriesOfAudioClips.GetClipsLength());
+
+            livingRoomDoor.GetComponent<DoorController>().EnableDoor();
+            livingRoomDoor.GetComponent<DoorController>().Use();
+        }
+
+        IEnumerator FlashLights()
+        {
+            var startTime = Time.time;
+            while (Time.time < startTime + playSeriesOfAudioClips.GetClipsLength())
+            {
+                var lightTime = Random.Range(0.1f, 0.5f);
+                flashingLight.enabled = !flashingLight.enabled;
+                yield return new WaitForSeconds(lightTime);
+            }
+            flashingLight.enabled = false;
+        }
+    }
+}
